82c9d23cedfa8879530278dc010863149db1f8ce
rniel29 <ronannielmtl@gmail.com>
2024-05-07 15:51:27 +0200

    test le déplacement d'un joueur

--- NielFerreiradesousaProjet/src/test/java/modele/PlayerTest.java
+++ NielFerreiradesousaProjet/src/test/java/modele/PlayerTest.java
@@ -1,5 +1,6 @@
 package modele;
 
+import org.javatuples.Pair;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,11 @@ class PlayerTest {
 
     @Test
     void deplacement() {
+        Pair[] resultat = {new Pair<Integer,Integer>(3,0),new Pair<Integer,Integer>(0,3),new Pair<Integer,Integer>(-3,0),new Pair<Integer,Integer>(0,-3),new Pair<Integer,Integer>(3,3),new Pair<Integer,Integer>(-3,-3),new Pair<Integer,Integer>(3,-3),new Pair<Integer,Integer>(-3,3)}; // Les résultats attendus
+        Player joueurtest = new Player();
+        for (int i = 0; i < resultat.length; i++) {
+            boolean b=(joueurtest.deplacement(resultat[i]))==joueurtest.getPlayerPos();
+            assertTrue(b);
 
     }
 
