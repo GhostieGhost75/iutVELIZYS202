da8b4d422b13e8395004d4a24b3c6fb5e181b9c5
RONAN NIEL <22302110@dinfo.iut-velizy.uvsq.fr>
2024-05-07 13:53:17 +0200

    creation fichier de test

---
+++ NielFerreiradesousaProjet/src/test/java/modele/PlayerTest.java
@@ -0,0 +1,17 @@
+package modele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void deplacement() {
+
+    }
+
+    @Test
+    void permutation() {
+    }
+}
---
+++ NielFerreiradesousaProjet/src/test/java/modele/TempleTest.java
@@ -0,0 +1,13 @@
+package modele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TempleTest {
+    @Test
+    void Temple() {
+
+    }
+
+}
