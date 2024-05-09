38694104e3536f144078a1d0f565bd419b0db9cd
Ronan niel <ronannielmtl@gmail.com>
2024-05-09 09:33:19 +0200

    test la bonne création d'un temple

--- NielFerreiradesousaProjet/src/test/java/modele/TempleTest.java
+++ NielFerreiradesousaProjet/src/test/java/modele/TempleTest.java
@@ -1,5 +1,6 @@
 package modele;
 
+import org.javatuples.Pair;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class TempleTest {
     @Test
     void Temple() {
-
+        int numTemple=5;
+        int numCristal=2;
+        Pair positionTemple = new Pair<Integer,Integer>(5,5);
+        Temple templeTest= new Temple(new Pair<Integer,Integer>(5,5),2,5);
+        boolean test1 = templeTest.getPos().equals(positionTemple);
+        boolean test2 = templeTest.getCristal()==numCristal;
+        boolean test3 = templeTest.getNum()==numTemple;
+        assertTrue(test1);
+        assertTrue(test2);
+        assertTrue(test3);
     }
 
 }
