f8152d549f672ea842b4f89b61b2d00470074b72
Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2024-05-31 15:17:52 +0200

    Creation de l'algorithme TriInsert (niveau 1)

--- NielFerreiradesousaProjet/src/main/java/modele/Algorithmes.java
+++ NielFerreiradesousaProjet/src/main/java/modele/Algorithmes.java
@@ -1,4 +1,34 @@
 package modele;
 
+import java.util.LinkedList;
+import java.util.TreeMap;
+
 public class Algorithmes {
+
+    public static LinkedList<Position> TriInsert(Player parPlayer) {
+        TreeMap<Position, Temple> templeMap = parPlayer.getTemples();
+        Temple [] temples = new Temple[templeMap.keySet().size()];
+        LinkedList<Position> parcours = new LinkedList<>();
+        for (Position pos : parPlayer.getTemples().keySet())
+            temples[templeMap.get(pos).getNum()-1] = templeMap.get(pos);
+        for (int i=0; i< temples.length;i++) {
+            int min = i;
+            for (int j=i+1; j< temples.length;j++) {
+                if (temples[j].getCristal()<temples[min].getCristal())
+                    min = j;
+            }
+            if (min != i) {
+                parcours.add(temples[min].getPos());
+                parcours.add(temples[i].getPos());
+                parcours.add(temples[min].getPos());
+                Temple temp = temples[i];
+                temples[i] = temples[min];
+                temples[min] = temp;
+
+            }
+        }
+        System.out.println(temples);
+        System.out.println(parcours);
+        return parcours;
+    }
 }
