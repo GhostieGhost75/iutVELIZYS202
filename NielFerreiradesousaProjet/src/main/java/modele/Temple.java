14a980e10a013e8e465126d5676b455a4f798aad
GhostieGhost75 <lucassousa2005.ls@gmail.com>
2024-06-02 20:30:11 +0200

    Erreur corrigée dans le constructeur du Temple : cristal et numéro de temple inversés

--- NielFerreiradesousaProjet/src/main/java/modele/Temple.java
+++ NielFerreiradesousaProjet/src/main/java/modele/Temple.java
@@ -7,7 +7,7 @@ public class Temple {
     private int cristalCol;
     private final int templeNum;
 
-    public Temple(Position parPos, int parCris, int parNum) {
+    public Temple(Position parPos, int parNum, int parCris) {
         templePos = parPos;
         cristalCol = parCris;
         templeNum = parNum;
