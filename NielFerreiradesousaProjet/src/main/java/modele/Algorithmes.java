56682d494807b4e85fd6c90ce0ba361a54c7beb4
Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2024-06-03 10:50:22 +0200

    Revert "Ajout de l'algorithme heuristique (les tests seront commit plus tard car la branche Main n'a pas encore été merge), modification du toString de Position"
    This reverts commit fadc9e00c61de08951446d175d47ac82f348b6b9.

--- NielFerreiradesousaProjet/src/main/java/modele/Algorithmes.java
+++ NielFerreiradesousaProjet/src/main/java/modele/Algorithmes.java
@@ -9,7 +9,7 @@ public class Algorithmes {
         TreeMap<Position, Temple> templeMap = parPlayer.getTemples();
         Temple [] temples = new Temple[templeMap.keySet().size()];
         LinkedList<Position> parcours = new LinkedList<>();
-        for (Position pos : templeMap.keySet())
+        for (Position pos : parPlayer.getTemples().keySet())
             temples[templeMap.get(pos).getNum()-1] = templeMap.get(pos);
         for (int i=0; i< temples.length;i++) {
             int min = i;
@@ -27,33 +27,7 @@ public class Algorithmes {
 
             }
         }
-        System.out.println(parcours);
-        return parcours;
-    }
-
-    public static LinkedList<Position> Heuristique(Player parPlayer) {
-        TreeMap<Position, Temple> templeMap = parPlayer.getTemples();
-        TreeMap<Integer, Temple> temples = new TreeMap<>();
-        LinkedList<Position> parcours = new LinkedList<>();
-        for (Position pos : templeMap.keySet()) {
-            if (templeMap.get(pos).getNum() != templeMap.get(pos).getCristal())
-                temples.put(templeMap.get(pos).getNum(), templeMap.get(pos));
-        }
-        while (!temples.isEmpty()) {
-            Temple first = temples.get(temples.firstKey());
-            parcours.add(first.getPos());
-            Temple next = temples.get(first.getCristal());
-            Temple previous;
-            while (temples.get(next.getCristal()) != null) {
-                System.out.println(next);
-                System.out.println(temples);
-                parcours.add(next.getPos());
-                previous = next;
-                next = temples.get(previous.getCristal());
-                if (previous.getCristal() == next.getNum())
-                    temples.remove(next.getNum());
-            }
-        }
+        System.out.println(temples);
         System.out.println(parcours);
         return parcours;
     }
