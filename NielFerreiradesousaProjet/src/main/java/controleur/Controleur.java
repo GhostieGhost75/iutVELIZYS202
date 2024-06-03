f7586dae512863134e4f657f2bc0bef8f6c6b635
Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2024-06-03 14:49:31 +0200

    Correction des tests pour TriSelect, ajout des tests pour l'algorithme Heuristique et la longueur du parcours

--- NielFerreiradesousaProjet/src/main/java/controleur/Controleur.java
+++ NielFerreiradesousaProjet/src/main/java/controleur/Controleur.java
@@ -34,7 +34,6 @@ public class Controleur implements EventHandler {
                 System.out.println("Temple à la position : " + temples.get(temple).getPos());
                 vboxCanva.dessinerTemple(temples.get(temple));
             }
-            Algorithmes.Heuristique(joueur);
         }
     }
     public void handleMenuItemActionManuel(Event event){
--- NielFerreiradesousaProjet/src/test/java/modele/AlgoTest.java
+++ NielFerreiradesousaProjet/src/test/java/modele/AlgoTest.java
@@ -12,21 +12,54 @@ public class AlgoTest {
     @Test
     void TriSelect() {
         Player joueur = new Player();
-        TreeMap<Position, Temple> templesTest = LectureScenario.lecture(new File("maps/scenario1.txt"));
+        TreeMap<Position, Temple> templesTest = LectureScenario.lecture(new File("maps/scenario0.txt"));
+        joueur.setTemples(templesTest);
         LinkedList<Position> posVoulues = new LinkedList<>();
         Position [] posVouluesTab = {
-                new Position(10,10),new Position(15,3),
-                new Position(10,10),new Position(15,3),
-                new Position(12,4),new Position(15,3),
-                new Position(8,5),new Position(6,4),
-                new Position(8,5),new Position(6,4),
-                new Position(12,4),new Position(6,4),
+                new Position(14,18),new Position(14,14),
+                new Position(14,18),new Position(18,14),
+                new Position(14,14),new Position(18,14),
+                new Position(14,14),new Position(18,18),
+                new Position(14,14)
         };
         for (Position pos:posVouluesTab)
             posVoulues.add(pos);
         LinkedList<Position> resultatSelect = Algorithmes.TriSelect(joueur);
         while (resultatSelect.size() != 0)
-            assert(resultatSelect.poll()==posVoulues.poll());
+            assert(resultatSelect.poll().equals(posVoulues.poll()));
+    }
+
+    @Test
+    void Heuristique() {
+        Player joueur = new Player();
+        TreeMap<Position, Temple> templesTest = LectureScenario.lecture(new File("maps/scenario1.txt"));
+        joueur.setTemples(templesTest);
+        LinkedList<Position> posVoulues = new LinkedList<>();
+        Position [] posVouluesTab = {
+                new Position(21,19),new Position(23,20),
+                new Position(27,19),new Position(30,18),
+                new Position(25,25),new Position(21,19),
+        };
+        for (Position pos:posVouluesTab)
+            posVoulues.add(pos);
+        LinkedList<Position> resultatHeuristique = Algorithmes.Heuristique(joueur);
+        while (resultatHeuristique.size() != 0)
+            assert(resultatHeuristique.poll().equals(posVoulues.poll()));
+    }
+
+    @Test
+    void longueurParcoursAlgo() {
+        TreeMap<Position, Temple> templesTest1 = LectureScenario.lecture(new File("maps/scenario1.txt"));
+        TreeMap<Position, Temple> templesTest2 = LectureScenario.lecture(new File("maps/scenario1.txt"));
+        Player joueur1 = new Player();
+        joueur1.setTemples(templesTest1);
+        Player joueur2 = new Player();
+        joueur2.setTemples(templesTest2);
+        LinkedList<Position> resultatSelect = Algorithmes.TriSelect(joueur1);
+        LinkedList<Position> resultatHeuristique = Algorithmes.Heuristique(joueur2);
+        assert(Algorithmes.longueurParcoursAlgo(resultatSelect) == 98);
+        assert(Algorithmes.longueurParcoursAlgo(resultatHeuristique) == 44);
+
     }
 
 }
