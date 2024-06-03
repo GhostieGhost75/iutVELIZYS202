d9845952a2a4317993ea28fe887f26bdad46216a
Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2024-06-03 11:02:14 +0200

    Controleur de la branche graphics (suite à une erreur de merge faite par Ferreira de Sousa)

--- NielFerreiradesousaProjet/src/main/java/controleur/Controleur.java
+++ NielFerreiradesousaProjet/src/main/java/controleur/Controleur.java
@@ -1,9 +1,11 @@
 package controleur;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.MenuItem;
 import modele.*;
+import vue.StackpaneMode;
 import vue.VBoxRoot;
 import vue.VboxCanva;
 
@@ -19,18 +21,45 @@ public class Controleur implements EventHandler {
             System.out.println("Fichier sélectionné : " + fichierScenario.getName());
             TreeMap<Position, Temple> temples = LectureScenario.lecture(fichierScenario);
             System.out.println("Nombre de temples lus : " + temples.size());
-            VBoxRoot.getApprenti().setTemples(temples);
+            Player joueur = VBoxRoot.getApprenti();
+            joueur.setTemples(temples);
             VboxCanva vboxCanva = VBoxRoot.getVboxCanva();
             vboxCanva.effacerTout();
-            Player joueur = VBoxRoot.getApprenti();
+            joueur.reset();
+            vboxCanva.labelNombreDePas.setText("Nombre de pas : " + joueur.getNombreDePas());
             vboxCanva.cliquemouv(new Position(joueur.getAbscisse(), joueur.getOrdonnee()), joueur);
+            vboxCanva.effacerTout();
             vboxCanva.afficherJoueur(joueur);
-
             for (Position temple : temples.keySet()) {
                 System.out.println("Temple à la position : " + temples.get(temple).getPos());
                 vboxCanva.dessinerTemple(temples.get(temple));
             }
+
+
         }
     }
+    public void handleMenuItemActionManuel(Event event){
+        StackpaneMode stackpaneMode = VBoxRoot.getStackpaneMode();
+        if (stackpaneMode.getChildren().get(0).equals(stackpaneMode.getVueModemanuel())){
+            stackpaneMode.getChildren().get(0).toFront();}
+        System.out.println(stackpaneMode.getChildren());
 
+
+    }
+    public void handleMenuItemActionHeuristique(Event event){
+        StackpaneMode stackpaneMode = VBoxRoot.getStackpaneMode();
+        if (stackpaneMode.getChildren().get(0).equals(stackpaneMode.getvuemodeHeuristique())) {
+            stackpaneMode.getChildren().get(0).toFront();
+        }
+        System.out.println(stackpaneMode.getChildren());
+
+    }
+
+    public void handleButtonClick(ActionEvent event) {
+        VboxCanva vboxCanva = VBoxRoot.getVboxCanva();
+        Player joueur = VBoxRoot.getApprenti();
+        joueur.permutation();
+        vboxCanva.afficherJoueur(joueur);
+
+    }
 }
